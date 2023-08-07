5ae864c4f56ca8e873963ba7d782217e9eeffbf3
Karl <karl.wasson@gmail.com>
2023-08-07 21:51:29 +0700

    update fetch

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     setLoading(true); // Set loading state to true when request starts
 
     try {
-      const response = await fetch('http://138.197.8.249:3000/api/generateOutline/generateOutline', {
+      const response = await fetch('138.197.8.249:3000/generateOutline', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
