48bc90388aea99f6a3cae7d178550e675f49ceae
Karl <karl.wasson@gmail.com>
2023-08-08 18:26:16 +0700

    update fetch

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     setLoading(true); // Set loading state to true when request starts
 
     try {
-      const response = await fetch('https://138.197.8.249:3000/generateOutline', {
+      const response = await fetch('https://otd.zip/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
